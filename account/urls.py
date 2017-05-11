9ae84f9be083807d34a445d7fc48f53e086c0788
John Rees <johnleonardrees@gmail.com>
2017-05-11 20:34:03 +0100

    Added password changing
    Added password change feature

--- account/urls.py
+++ account/urls.py
@@ -7,5 +7,13 @@ urlpatterns = [
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^logout-then-login/$', auth_views.login, name='logout_then_login'),
-    url(r'^$', views.dashboard, name='dashboard')
+    url(r'^$', views.dashboard, name='dashboard'),
+
+    # change password urls
+    url(r'^password-change/$', auth_views.password_change,
+        name='password_change'),
+    url(r'^password-change/done/$',
+        auth_views.password_change_done,
+        name="password_change_done"),
+
 ]
