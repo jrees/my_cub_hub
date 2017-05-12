50442128badcf2a6743861890bab665de67017f2
John Rees <johnleonardrees@gmail.com>
2017-05-12 21:00:30 +0100

    Added password reset feature
    Need to implement proper email functionality for deployment though.

--- account/urls.py
+++ account/urls.py
@@ -16,4 +16,17 @@ urlpatterns = [
         auth_views.password_change_done,
         name="password_change_done"),
 
-]
+    # restore passwordd urls
+    url(r'^password-reset/$',
+        auth_views.password_reset,
+        name="password_reset"),
+    url(r'^password-reset/done/$',
+        auth_views.password_reset_done,
+        name="password_reset_done"),
+    url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$',
+        auth_views.password_reset_confirm,
+        name='password_reset_confirm'),
+    url(r'^password-reset/complete/$',
+        auth_views.password_reset_complete,
+        name="password_reset_complete"),
+    ]
