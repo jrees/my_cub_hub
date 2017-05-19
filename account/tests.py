666fb2b853b86e9267983e74d13e312a59dd40d4
John Rees <johnleonardrees@gmail.com>
2017-05-19 06:40:01 +0100

    First unittest
    tests that dashboard view is used when visiting /account/

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from .views import dashboard
+
 
 # Create your tests here.
+class UrlTests(TestCase):
+
+    def test_root_url(self):
+        found = resolve('/')
+        # line below uses the url_name, whereas the line after uses func, both seem to work
+        # self.assertEqual(found.url_name, 'dashboard')
+        self.assertEquals(found.func, dashboard)
